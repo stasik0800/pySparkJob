2ea78bceea1e812031a02d4135b9f24502eb2891
stasik0800 <tsvayg1@gmail.com>
2020-07-13 20:17:18 +0300

    modify readme and config file

--- config.py
+++ config.py
@@ -1,8 +1,24 @@
 from pyspark.sql.types import *
 import os
 
+def createDir(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    return dir
+
+def getDFSparkSchema():
+    return StructType([
+        StructField("url", StringType()),
+        StructField("user_id", StringType()),
+        StructField("timestamp", StringType()),
+        StructField("type", StringType()),
+        StructField("sessionid", IntegerType())])
+
+
+
+
 # app config
-os.environ['HADOOP_HOME'] = "C:\\winutils\\hadoop-2.2.0"
+# os.environ['HADOOP_HOME'] = "C:\\winutils\\hadoop-2.2.0"
 sparkAppName = "Elementor"
 encoding = "UTF-8"
 
@@ -13,19 +29,12 @@ baseTblName ='baseTbl'
 analyticsDf= 'df'
 
 
-
 # out files location
 projectLocation = os.path.dirname(os.path.abspath(__file__))
-errorlogOut = os.path.join(projectLocation,'out\\errorlog')
-resultsOut = os.path.join(projectLocation, 'out\\results')
+errorlogOut = createDir(os.path.join(projectLocation,'out\\errorlog'))
+resultsOut = createDir(os.path.join(projectLocation, 'out\\results'))
 datalocation = os.path.join(projectLocation, 'data\\StructuredStreamingText.txt')
 csvFormat = '.csv'
 
 
-def getDFSparkSchema():
-    return StructType([
-        StructField("url", StringType()),
-        StructField("user_id", StringType()),
-        StructField("timestamp", StringType()),
-        StructField("type", StringType()),
-        StructField("sessionid", IntegerType())])
+
